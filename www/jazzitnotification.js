9594330e64174544eb49c4681978d1247673b471
alisonsilva <alison@laminarsoft.com.br>
2014-10-08 22:12:03 -0300

    showMessage
    Correção de alguns defeitos e inclusão do método showMessage

--- www/jazzitnotification.js
+++ www/jazzitnotification.js
@@ -5,6 +5,12 @@ var jazzitnotification = {
     },
     goBackground: function(successCallback, failureCallback) {
         cordova.exec(successCallback, failureCallback, 'JazzitNotificationPlugin', 'goBackground', []);
+    },
+    showMessage : function(successCallback, failureCallback, notificationId, options) {
+        if (notificationId == '') {
+            notificationId = Math.floor(Math.random()*10000000000).toString();
+        }
+        cordova.exec(successCallback, failureCallback, 'JazzitNotificationPlugin', 'showMessage', [notificationId, options]);
     }
 };
 

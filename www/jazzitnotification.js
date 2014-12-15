25df8eeee8dc1f9f94575625a3f55f776d69be98
Alison <alisonsilva123@gmail.com>
2014-12-15 10:07:32 -0200

    Aleração dos nomes dos métodos javascript

--- www/jazzitnotification.js
+++ www/jazzitnotification.js
@@ -3,32 +3,32 @@ var exec = require('cordova/exec');
 
 module.exports = {
 
-    goHome : function(successCallback, failureCallback) {
+    jazzGoHome : function(successCallback, failureCallback) {
         exec(successCallback, failureCallback, 'JazzitNotificationPlugin', 'goHome', []);
     },
-    goBackground: function(successCallback, failureCallback) {
+    jazzGoBackground: function(successCallback, failureCallback) {
         exec(successCallback, failureCallback, 'JazzitNotificationPlugin', 'goBackground', []);
     },
-    showMessage : function(successCallback, failureCallback, notificationId, options) {
+    jazzShowMessage : function(successCallback, failureCallback, notificationId, options) {
         if (notificationId == '') {
             notificationId = Math.floor(Math.random()*10000000000).toString();
         }
         exec(successCallback, failureCallback, 'JazzitNotificationPlugin', 'showMessage', [notificationId, options]);
     },
-    storeFile : function(successCallback, failureCallback, options) {
+    jazzStoreFile : function(successCallback, failureCallback, options) {
         exec(successCallback, failureCallback, 'JazzitNotificationPlugin', 'storeFile', [options]);
     },
-    openFile : function(successCallback, failureCallback, options) {
+    jazzOpenFile : function(successCallback, failureCallback, options) {
         exec(successCallback, failureCallback, 'JazzitNotificationPlugin', 'openFile', [options]);
     },
-    retrieveAndShowFile : function(successCallback, failureCallback, options) {
+    jazzRetrieveAndShowFile : function(successCallback, failureCallback, options) {
         exec(successCallback, failureCallback, 'JazzitNotificationPlugin', 'retrieveAndShowFile', [options]);
     },
     
     /**
      * Immediately cancels any currently running vibration.
      */
-    exibirMensagem: function() {
+    jazzExibirMensagem: function() {
         exec(null, null, "JazzitTest", "exibirMensagem", []);
     }
 };
